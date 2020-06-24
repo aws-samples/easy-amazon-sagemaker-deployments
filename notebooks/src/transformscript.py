b185640d47582d52e12ce5440adb8cdfe66bbe0a
EC2 Default User <ec2-user@ip-172-16-13-112.us-east-2.compute.internal>
2020-06-24 02:30:10 +0000

    fixed notebooks

--- notebooks/src/transformscript.py
+++ notebooks/src/transformscript.py
@@ -1,25 +1,43 @@
-import sklearn
+import gluonnlp as nlp; import mxnet as mx;
 from joblib import load
 import numpy as np
 import os
+import json
 
 #Return loaded model
 def load_model(modelpath):
-    print(modelpath)
-    clf = load(os.path.join(modelpath,'model.joblib'))
+    model, vocab = nlp.model.get_model('distilbert_6_768_12', dataset_name='distilbert_book_corpus_wiki_en_uncased');
     print("loaded")
-    return clf
+    return {'model':model,'vocab':vocab}
 
 # return prediction based on loaded model (from the step above) and an input payload
-def predict(model, payload):
-    try:
-        # locally, payload may come in as an np.ndarray
-        if type(payload)==np.ndarray:
-            out = [str(model.predict(np.frombuffer(payload).reshape((1,64))))]
-        # in remote / container based deployment, payload comes in as a stream of bytes
-        else:
-            out = [str(model.predict(np.frombuffer(payload[0]['body']).reshape((1,64))))]
-    except Exception as e:
-        out = [type(payload),str(e)] #useful for debugging!
+def predict(modeldict, payload):
     
-    return out
+    #set_trace()
+    
+    model = modeldict['model']
+    vocab = modeldict['vocab']
+    
+    tokenizer = nlp.data.BERTTokenizer(vocab, lower=True);
+    transform = nlp.data.BERTSentenceTransform(tokenizer, max_seq_length=512, pair=False, pad=False);
+    
+    try:
+        # Local
+        if type(payload) == str:
+            sample = transform(payload);
+        elif type(payload) == bytes :
+            sample = transform(str(payload.decode()));
+        # Remote, standard payload comes in as a list of json strings with 'body' key
+        elif type(payload)==list:
+            sample = transform(payload[0]['body'].decode());
+        else:
+            return [json.dumps({'response':"Provide string or bytes string",
+                    'payload':str(payload),
+                    'type':str(type(payload))})]
+        
+        words, valid_len = mx.nd.array([sample[0]]), mx.nd.array([sample[1]])
+        out = model(words, valid_len)  
+        out = json.dumps({'output':out.asnumpy().tolist()})
+    except Exception as e:
+        out = str(e) #useful for debugging!
+    return [out]
