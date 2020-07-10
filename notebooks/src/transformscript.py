b3710e82ec494a9419ed15f403ad9861cfce5cd6
EC2 Default User <ec2-user@ip-172-16-32-142.ec2.internal>
2020-07-10 14:41:27 +0000

    changed pytorch example

--- notebooks/src/transformscript.py
+++ notebooks/src/transformscript.py
@@ -1,25 +1,57 @@
-import sklearn
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data
+import torch.utils.data.distributed
 from joblib import load
 import numpy as np
 import os
+import json
+from six import BytesIO
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+    
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #Return loaded model
 def load_model(modelpath):
-    print(modelpath)
-    clf = load(os.path.join(modelpath,'model.joblib'))
+    model = torch.nn.DataParallel(Net())
+    with open(os.path.join(modelpath, 'model.pth'), 'rb') as f:
+        model.load_state_dict(torch.load(f))
     print("loaded")
-    return clf
+    return model.to(device)
 
 # return prediction based on loaded model (from the step above) and an input payload
 def predict(model, payload):
-    try:
-        # locally, payload may come in as an np.ndarray
-        if type(payload)==np.ndarray:
-            out = [str(model.predict(np.frombuffer(payload).reshape((1,64))))]
-        # in remote / container based deployment, payload comes in as a stream of bytes
-        else:
-            out = [str(model.predict(np.frombuffer(payload[0]['body']).reshape((1,64))))]
-    except Exception as e:
-        out = [type(payload),str(e)] #useful for debugging!
     
-    return out
+    if type(payload) == list:
+        data = np.frombuffer(payload[0]['body'],dtype=np.float32).reshape(1,1,28,28)
+    elif type(payload) == np.ndarray:
+        data = payload  
+    try:
+        print(type(data))
+        input_data = torch.Tensor(data)
+        model.eval()
+        with torch.no_grad():
+            out =  model(input_data.to(device)).argmax(axis=1)[0].tolist()
+    except Exception as e:
+        out = str(e)
+    return [out]
