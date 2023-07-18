8a5ed510a00e581ef04b9ffc7e0bf7dde2d96e29
EC2 Default User <ec2-user@ip-172-16-3-168.ec2.internal>
2023-07-18 18:20:59 +0000

    added notebook

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras = {
 }
 
 setup(name='ezsmdeploy',
-      version='1.95dev',
+      version='1.96dev',
       description='SageMaker custom deployments made easy',
       url='https://pypi.python.org/pypi/ezsmdeploy',
       #scripts=['Dockerfile','dockerd-entrypoint.py','model_handler.py','build-docker.sh'],
