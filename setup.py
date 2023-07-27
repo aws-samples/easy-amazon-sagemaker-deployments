d80b718591ff4f14079eee59a61bfa8f21e63666
w601sxs <ec2-user@ip-172-16-3-168.ec2.internal>
2023-07-27 20:26:09 +0000

    cleaning example notebooks

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras = {
 }
 
 setup(name='ezsmdeploy',
-      version='1.97dev',
+      version='2.0.0',
       description='SageMaker custom deployments made easy',
       url='https://pypi.python.org/pypi/ezsmdeploy',
       #scripts=['Dockerfile','dockerd-entrypoint.py','model_handler.py','build-docker.sh'],
