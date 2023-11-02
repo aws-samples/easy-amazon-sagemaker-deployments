4e2f030a6de9a3525cbc6e54980843178636ebef
EC2 Default User <ec2-user@ip-172-16-120-114.ec2.internal>
2023-11-02 01:08:58 +0000

    Added langsam notebook

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras = {
 }
 
 setup(name='ezsmdeploy',
-      version='2.0.5',
+      version='2.0.6',
       description='SageMaker custom deployments made easy',
       url='https://pypi.python.org/pypi/ezsmdeploy',
       #scripts=['Dockerfile','dockerd-entrypoint.py','model_handler.py','build-docker.sh'],
